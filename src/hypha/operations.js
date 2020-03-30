b11e273c9b4c01a716ec97a41c9a7212f7928923
r-ba <rhysbalevicius@gmail.com>
2020-03-30 17:28:21 -0400

    Add operation library

---
+++ src/hypha/operations.js
@@ -0,0 +1,48 @@
+const Operations = {
+
+  // Arithmetical operators
+  '+' : (x, y) => x + y,
+  '-' : (x, y) => x - y,
+  '*' : (x, y) => x * y,
+  '/' : (x, y) => x / y,
+  '**' : (x, y) => x ** y,
+  '%' : (x, y) => x % y,
+
+  // Bitwise operators
+  '^' : (x, y) => x ^ y,
+  '&' : (x, y) => x & y,
+  '|' : (x, y) => x | y,
+  '<<' : (x, y) => x << y,
+  '>>' : (x, y) => x >> y,
+  '>>>' : (x, y) => x >>> y,
+  '~' : x => ~x,
+
+  // Logical operators
+  '&&' : (x, y) => x && y,
+  '||' : (x, y) => x || y,
+  '!' : x => !x,
+
+  // Comparison operators
+  '==' : (x, y) => x == y,
+  '===' : (x, y) => x === y,
+  '!=' : (x, y) => x != y,
+  '!==' : (x, y) => x !== y,
+  '!=' : (x, y) => x == y,
+  '<' : (x, y) => x < y,
+  '<=' : (x, y) => x <= y,
+  '>' : (x, y) => x > y,
+  '>=' : (x, y) => x >= y,
+
+  // Math library
+  'abs' : x => Math.abs(x),
+  'floor' : x => Math.floor(x),
+  'max' : function() {
+    return Math.max(...arguments);
+  },
+  'min' : function() {
+    return Math.min(...arguments);
+  },
+  // ...
+};
+
+export default Operations;
