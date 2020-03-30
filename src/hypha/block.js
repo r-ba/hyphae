9e8c4434030d1af61c6c7124eea1f8dd1b557f75
r-ba <rhysbalevicius@gmail.com>
2020-03-30 18:18:23 -0400

    Add defineStatements method to the prototype of Block

--- src/hypha/block.js
+++ src/hypha/block.js
@@ -3,11 +3,12 @@ import Operations from './operations.js'
 /*
  * Emulate a block statement
  */
-function Block(parent, scope, statements) {
-  if (parent)
+function Block(parent, scope, statements = []) {
+  if (parent) {
     Object.setPrototypeOf(this, parent);
+  }
   this.scope = scope || {};
-  this.statements = statements || [];
+  this.defineStatements(statements);
 };
 
 
@@ -15,12 +16,14 @@ function Block(parent, scope, statements) {
  *
  */
 Block.prototype.get = function(property) {
-  if (!this.scope)
+  if (!this.scope) {
     return undefined;
-  else {
-    if (property in this.scope)
+  } else {
+    if (property in this.scope) {
       return this.scope[property];
-    else return Object.getPrototypeOf(this).get(property);
+    } else {
+      return Object.getPrototypeOf(this).get(property);
+    }
   }
 };
 
@@ -29,13 +32,14 @@ Block.prototype.get = function(property) {
  *
  */
 Block.prototype.assign = function(property, value, origin) {
-  if (!this.scope)
+  if (!this.scope) {
     origin.scope[property] = value;
-  else {
-    if (property in this.scope)
+  } else {
+    if (property in this.scope) {
       this.scope[property] = value;
-    else
+    } else {
       Object.getPrototypeOf(this).assign(property, value, origin || this);
+    }
   }
 };
 
@@ -66,16 +70,38 @@ Block.prototype[Symbol.asyncIterator] = function() {
   return {
     next: () => {
       if (currStatement < numStatements) {
-        const { f, argv, to } = this.statements[currStatement++];
-        const output = Operations[f](...this.collect(argv));
-        this.assign(to, output);
-
-        return Promise.resolve({ value : output, done : false });
+        return Promise.resolve({
+          value : this.statements[currStatement++].execute(),
+          done : false
+        });
       } else {
         return Promise.resolve({ done : true });
       }
     }
   };
+};
+
+
+/*
+ *
+ */
+Block.prototype.defineStatements = function(statements) {
+  this.statements = statements.map(statement => {
+    if (statement instanceof Block) {
+      return statement;
+    } else {
+      const execute = () => {
+        const { f, argv, to } = statement;
+        let output = Operations[f](...this.collect(argv));
+        this.assign(to, output);
+        return output;
+      }
+
+      return {
+        execute : execute.bind(this)
+      };
+    }
+  });
 };
 
 
