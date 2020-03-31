471ba7eca7c344b7a1f34b8a40251255ab8c0b21
r-ba <rhysbalevicius@gmail.com>
2020-03-31 18:07:32 -0400

    Add documentation to LoopBlock

--- src/hypha/loop.js
+++ src/hypha/loop.js
@@ -1,17 +1,31 @@
 import Block from './block.js';
 import Operations from './operations.js';
 
+/**
+ * Emulate a while loop.
+ *
+ * @param {Block} parent The enclosing Block statement.
+ * @param {Array<object>=} statements The body of the loop.
+ * @param {Array<object>=} conditions The execution conditions.
+ */
 function LoopBlock(parent, statements=[], conditions=[]) {
   this.body = new Block(parent, {}, statements);
   this.conditions = conditions;
 };
 
 
+/**
+ * Continuously execute a body of statements until at least one condition
+ * evaluates to false.
+ */
 LoopBlock.prototype.execute = async function() {
   while (await this.checkCondition()) this.body.execute();
 };
 
 
+/**
+ * Evaluate a logical AND of the sequence of conditions.
+ */
 LoopBlock.prototype.checkCondition = async function() {
   for (const { f, argv } of this.conditions) {
     if (!Operations[f](...this.body.collect(argv))) {
