1fe0cdc7b0be5431016dafa0705d242f74448406
r-ba <rhysbalevicius@gmail.com>
2020-03-31 18:15:15 -0400

    Export Block objects from hypha/index.js

--- src/hypha/index.js
+++ src/hypha/index.js
@@ -1,24 +1,9 @@
 import Block from './block.js';
+import ConditionalBlock from './conditional.js';
+import LoopBlock from './loop.js';
 
-// example usage
-const parent = null;
-const scope = { a: 1, b: 2 };
-const statements = [
-  {
-    f : '+',
-    argv : [ 'a', 'b' ],
-    to : 'c'
-  },
-  {
-    f : '+',
-    argv : [ 'c', 'c' ],
-    to : 'c'
-  },
-];
-
-
-(async function() {
-  const b1 = new Block(parent, scope, statements);
-  await b1.execute();
-  console.log(b1.scope);
-})();
+export {
+  Block,
+  ConditionalBlock,
+  LoopBlock
+};
