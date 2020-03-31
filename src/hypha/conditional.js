fe14bff9add844991a819777753ef5f674ba43d0
r-ba <rhysbalevicius@gmail.com>
2020-03-31 18:01:53 -0400

    Add documentation to ConditionalBlock

--- src/hypha/conditional.js
+++ src/hypha/conditional.js
@@ -1,6 +1,12 @@
 import Operations from './operations.js';
 
-
+/**
+ * Emulate a conditional statement.
+ *
+ * @param {Block} parent The enclosing Block statement.
+ * @param {Array<Block>=} statements A sequence of n Block-like statements.
+ * @param {Array<object>=} conditions A sequence of n-1 conditional statements.
+ */
 function ConditionalBlock(parent, statements=[], conditions=[]) {
   this.parent = parent;
   this.statements = statements;
@@ -8,11 +14,18 @@ function ConditionalBlock(parent, statements=[], conditions=[]) {
 };
 
 
+/**
+ * Execute the first statement whose corresponding condition evaulates to true.
+ */
 ConditionalBlock.prototype.execute = async function() {
   for await (const statement of this) {};
 };
 
 
+/**
+ * Define an asynchronous flow of execution such that the potential evaluation
+ * of statement i is guaranteed to occur before statement j for all i < j.
+ */
 ConditionalBlock.prototype[Symbol.asyncIterator] = function() {
   let last = this.conditions.length;
   let current = -1;
