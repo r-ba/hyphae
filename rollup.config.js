243b7352d0fa1e05793b04dd8ee39723c142cd15
r-ba <rhysbalevicius@gmail.com>
2020-04-02 23:24:13 -0400

    Update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,13 @@
+import { terser } from 'rollup-plugin-terser';
+
 export default {
-  input: './src/index.js',
-  output: {
-    file: 'build/hyphae.umd.js',
-    name: 'hyphae',
-    format: 'umd'
-  }
+  input : './src/index.js',
+  output : {
+    file : 'build/hyphae.umd.js',
+    name : 'Hyphae',
+    format : 'umd'
+  },
+  plugins : [
+    terser()
+  ]
 };
