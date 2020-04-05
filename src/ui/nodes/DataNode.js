4c565183bd973d34036faa5001ef848e3da3bd15
r-ba <rhysbalevicius@gmail.com>
2020-04-04 21:29:59 -0400

    Add connectNode method to DataNode

--- src/ui/nodes/DataNode.js
+++ src/ui/nodes/DataNode.js
@@ -13,3 +13,40 @@ function DataNode(position, value) {
 /* Inherit from Node */
 DataNode.prototype = Object.create(Node.prototype);
 DataNode.prototype.constructor = DataNode;
+
+
+/**
+ * Setup the appropriate logic between this node and some target node.
+ *
+ * @param {object} target The Cytoscape target node.
+ * @param {object} edge The added Cytoscape edge object.
+ */
+DataNode.prototype.connectNode = function(target, edge) {
+  const {
+    type,
+    index,
+    connected,
+    targetType,
+    targetId
+  } = target.data();
+  let invalidConnection = true;
+
+  if (type === 'connector' && !connected) {
+    if (targetType === 'operation') {
+      NodeStore[targetType][targetId].options.argv[index] = this.id;
+      target.data('connected', true);
+      invalidConnection = false;
+    } else if (targetType === 'block' || targetType === 'loop') {
+      if (NodeStore[targetType][targetId].scope.indexOf(this.id) === -1) {
+        NodeStore[targetType][targetId].scope.push(this.id);
+        NodeStore[targetType][targetId].addConnector();
+        target.data('connected', true);
+        invalidConnection = false;
+      }
+    }
+  }
+
+  if (invalidConnection) {
+    cy.remove(edge);
+  }
+};
