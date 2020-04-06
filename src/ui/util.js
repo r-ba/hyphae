08ab4e4fcc92177f5f9712a0d07752e02602dca7
r-ba <rhysbalevicius@gmail.com>
2020-04-06 14:46:23 -0400

    Implement `positionConnectors` helper fn for radially spacing connector nodes

--- src/ui/util.js
+++ src/ui/util.js
@@ -9,6 +9,7 @@ const generateId = () => {
   return result;
 };
 
+
 const operatorArity = {
   // Arithmetical operators
   '+' : 2,
@@ -47,4 +48,27 @@ const operatorArity = {
   'floor' : 1,
   'max' : 2,
   'min' : 2,
+};
+
+
+const positionConnectors = (radii, position, connectors) => {
+  const { x, y } = position;
+  const n = connectors.length;
+  const a = -1 * Math.PI / (2 * n);
+  const r = radii.length;
+
+  for (let i = 0; i < n; i++) {
+    connectors[i].forEach((ele, j) => {
+      const x0 = Math.cos((i + n/2 + 0.5) * a);
+      const y0 = Math.sin((i + n/2 + 0.5) * a);
+      ele.animate({
+        position : {
+          x : x - radii[j] * x0,
+          y : y - radii[j] * y0
+        }
+      }, {
+        duration : 750
+      })
+    });
+  }
 };
