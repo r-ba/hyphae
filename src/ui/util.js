0b7e41646ab56ccf3c08cc91cad152c155c78fad
r-ba <rhysbalevicius@gmail.com>
2020-04-07 17:35:45 -0400

    Add `connectNode` helper function

--- src/ui/util.js
+++ src/ui/util.js
@@ -1,15 +1,83 @@
-// To-do: prevent or mitigate risk of collisions
-const generateId = () => {
-  let result = '';
-  const characters = '0123456789ABCDEF';
-  const charactersLength = characters.length;
-  for ( let i = 0; i < 16; i++ ) {
-    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+/**
+ * Radially space a sequence of connector nodes.
+ *
+ * @param {Array<number>} radii The radii to distance paired connectors.
+ * @param {object} position The position of the connector's parent node.
+ * @param {Array<object>} connectors
+ */
+const positionConnectors = (radii, position, connectors) => {
+  const { x, y } = position;
+  const n = connectors.length;
+  const a = -1 * Math.PI / (2 * n);
+  const r = radii.length;
+
+  for (let i = 0; i < n; i++) {
+    connectors[i].forEach((ele, j) => {
+      const x0 = Math.cos((i + n/2 + 0.5) * a);
+      const y0 = Math.sin((i + n/2 + 0.5) * a);
+      ele.animate({
+        position : {
+          x : x - radii[j] * x0,
+          y : y - radii[j] * y0
+        }
+      }, {
+        duration : 750
+      })
+    });
   }
-  return result;
 };
 
 
+/**
+ * Connect two blocks together, if permitted.
+ *
+ * @param {string} targetType
+ * @param {string} targetId
+ * @param {object} sourceBlock
+ * @param {string} sourceId
+ * @return {boolean} Return true iff the connection was successful.
+ */
+const connectNode = (targetData, sourceId, sourceBlock) => {
+  const {
+    type,
+    index,
+    midPoint,
+    connected,
+    targetType,
+    targetId
+  } = targetData;
+
+  let isSuccessfulConnection = false;
+
+  if (type === 'connector' && !connected && targetId !== sourceId) {
+    if (targetType === 'block') {
+      if (!NodeStore[targetType][targetId].hyphaeInstance.isDescendantOf(sourceBlock)) {
+        sourceBlock.defineParent(NodeStore[targetType][targetId].hyphaeInstance);
+        isSuccessfulConnection = true;
+      }
+
+    } else if (!midPoint) {
+      if (targetType === 'conditional' || targetType === 'loop') {
+        if (!NodeStore[targetType][targetId].hyphaeInstance.body.isDescendantOf(sourceBlock)) {
+          sourceBlock.defineParent(NodeStore[targetType][targetId].hyphaeInstance.body);
+          isSuccessfulConnection = true;
+        }
+      }
+    }
+  }
+
+  if (isSuccessfulConnection) {
+    NodeStore[targetType][targetId].addConnector();
+    cy.getElementById(sourceId).data('handleable', false);
+  }
+
+  return isSuccessfulConnection;
+};
+
+
+/**
+ * A lookup table mapping operation symbols to their required number arguments.
+ */
 const operatorArity = {
   // Arithmetical operators
   '+' : 2,
@@ -46,29 +114,6 @@ const operatorArity = {
   // Math library
   'abs' : 1,
   'floor' : 1,
-  'max' : 2,
-  'min' : 2,
-};
-
-
-const positionConnectors = (radii, position, connectors) => {
-  const { x, y } = position;
-  const n = connectors.length;
-  const a = -1 * Math.PI / (2 * n);
-  const r = radii.length;
-
-  for (let i = 0; i < n; i++) {
-    connectors[i].forEach((ele, j) => {
-      const x0 = Math.cos((i + n/2 + 0.5) * a);
-      const y0 = Math.sin((i + n/2 + 0.5) * a);
-      ele.animate({
-        position : {
-          x : x - radii[j] * x0,
-          y : y - radii[j] * y0
-        }
-      }, {
-        duration : 750
-      })
-    });
-  }
+  'max' : 2, // Can replace this with Infinity once proper infrastructure is in place
+  'min' : 2, // Same note as `max`
 };
