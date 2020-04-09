169c2cec25d6f2ca58e3c304e9403b800b310996
r-ba <rhysbalevicius@gmail.com>
2020-04-09 14:24:45 -0400

    Add listeners to BlockNode, ConditionalNode, and LoopNode to call connectNode on click

--- src/ui/listeners.js
+++ src/ui/listeners.js
@@ -1,5 +1,13 @@
 cy.on('click', '.block', function(event) {
-  console.log(event.target.id());
+  NodeStore.block[event.target.id()].addConnector();
+});
+
+cy.on('click', '.conditional', function(event) {
+  NodeStore.conditional[event.target.id()].addConnector();
+});
+
+cy.on('click', '.loop', function(event) {
+  NodeStore.loop[event.target.id()].addConnector();
 });
 
 cy.on('click', '.connector', function(event) {
