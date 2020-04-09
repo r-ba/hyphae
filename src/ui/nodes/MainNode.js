1d4e091742871d844edbde68d9ae58a67c8f8538
r-ba <rhysbalevicius@gmail.com>
2020-04-08 23:18:56 -0400

    Implement `compile` method on prototype of MainNode

--- src/ui/nodes/MainNode.js
+++ src/ui/nodes/MainNode.js
@@ -15,4 +15,67 @@ function MainNode(position) {
   });
 
   this.id = this.cyInstance.id();
+  this.connectors = [];
+  this.main = new Block();
+  this.isValidated = false;
+
+  // const { x, y } = position;
+  // const spawnTypes = [];
+  // for (const type of ['data', 'operation', 'block', 'conditional', 'loop']) {
+  //   spawnTypes.push(cy.add({
+  //     group : 'nodes',
+  //     grabbable : false,
+  //     position : {
+  //       x : x,
+  //       y : y
+  //     },
+  //     classes: [ `spawn${type}` ]
+  //   }));
+  // }
+  //
+  // positionConnectors([200], { x, y }, spawnTypes);
 }
+
+
+/**
+ * Collect data, validate subtrees, and set corresponding Hypha object properties.
+ */
+MainNode.prototype.compile = async function() {
+  const scope = {};
+  let statementIndex = 0;
+  let successStatus = false;
+
+  for (const connector of this.connectors) {
+    const { type, id } = cy.getElementById(connector).data()
+
+    if (type === 'data') {
+      scope[id] = NodeStore[type][id].value;
+    } else {
+      successStatus = await NodeStore[type][id].compile();
+      if (successStatus) {
+        let statement;
+        if (type === 'operation') {
+          statement = NodeStore[type][id].options;
+        } else {
+          statement = NodeStore[type][id].hyphaeInstance;
+        }
+        this.main.insertStatement(statementIndex++, statement);
+      } else {
+        break;
+      }
+    }
+  }
+
+  this.main.scope = scope;
+  this.isValidated = successStatus;
+};
+
+
+/**
+ * Execute the Main block iff it's been successfully compiled.
+ */
+MainNode.prototype.execute = async function() {
+  if (this.isValidated) {
+    await this.main.execute();
+  }
+};
